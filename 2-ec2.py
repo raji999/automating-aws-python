44733c880f7361c745b66a71933a98fd17ad7e2d
LuberCloudNinja <63034421+LuberCloudNinja@users.noreply.github.com>
2020-06-13 12:33:21 +1000

    Update 2-ec2.py

--- 2-ec2.py
+++ 2-ec2.py
@@ -1,54 +1,30 @@
-#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
-import boto3
-from pprint import pprint
-import sys
-import time
-
-lguilarte = boto3.session.Session(profile_name='lguilarte')
-ec2_cli = lguilarte.client(service_name='ec2', region_name='us-east-1')
-
 try:
     def main():
-        options = """We can execute one of the below actions:
-            1. If you want to start an instance please enter 1.
-            2. If you want to stop an instance please enter 2.
-            3. If you want to reboot an instance please enter 3.
-            4. If you want to terminate an instance please enter 4.
-            5. Enter 5 to exit."""
-        print("__________________________")
-        print(f"This script allows you to:\n1-list all your instances.\n2-Start instance/s.\n3-Stop instance/s.\n4-Restart instance/s\n5-Terminate instance/s.")
+        print(f"This script allows you to:\n-list all your instances.\n-Start an instance.\n-Stop an instance\n-Restart instance\n-Terminate instance/s.")
+        print("_______________________________")
         user_promt = input(
-            "Do you want to see all your instances before? Please enter yes, no or exit to finish: ")
-        if user_promt.lower() == "yes" or user_promt == "1":
+            "Do you want to see all your instances now? Please enter yes, no or exit to finish: ")
+        if user_promt.lower() == "yes" or user_promt.lower() == "y":
             list_instance()
-            print(options)
-            select = input("Please enter one of the option: ")
-            if select == "1" or select.lower() == "one":
-                start_ec2s()
-            elif select == "2" or select.lower() == "two":
-                stop_ec2s()
-            elif select == "3" or select.lower() == "reboot":
-                reboot_ec2s()
-            elif select == "4" or select.lower() == "terminate":
-                terminate_ec2()
-        elif user_promt == "2" or user_promt.lower() == "no":
+            print(
+                "___________________________________________________________________________")
+            menu()
+        elif user_promt.lower() == "no":
             time.sleep(0.7)
-            print(options)
-            select = input("Please chose one of the option: ")
-            if select == "1" or select.lower() == "one":
-                start_ec2s()
-            elif select == "2" or select.lower() == "two":
-                stop_ec2s()
-            elif select == "3" or select.lower() == "reboot":
-                reboot_ec2s()
-            elif select == "4" or select.lower() == "terminate":
-                terminate_ec2()
+            menu()
+
         elif user_promt.lower() == "exit":
             print("Goodbye......")
             sys.exit()
         else:
-            print(
-                "Sorry, You did not enter one of the five options. You can run the script again.\nGoodBye.....")
+            print("Sorry, your entry was not valid.")
+            try_again = input("Do you want to try again?: ")
+            if try_again == "1" or try_again.lower() == "yes":
+                main()
+            else:
+                print("GoodBye.....")
+                sys.exit()
+
         return user_promt
 
     def list_instance():
@@ -63,8 +39,38 @@ try:
                     f"{num}-Instance ID: {values['InstanceId']}, is currently {values['State']['Name']} in AZ: {values['Placement']['AvailabilityZone']}.")
         return None
 
+    def menu():
+        options = """Now we can execute one of the below actions:
+            1. If you want to start an instance please enter 1.
+            2. If you want to stop an instance please enter 2.
+            3. If you want to reboot an instance please enter 3.
+            4. If you want to terminate an instance please enter 4.
+            5. Enter 5 to exit."""
+        print("___________________________________________________________________________")
+        print(options)
+        select = input("Please enter one of the option: ")
+        if select == "1" or select.lower() == "one":
+            start_ec2s()
+        elif select == "2" or select.lower() == "two":
+            stop_ec2s()
+        elif select == "3" or select.lower() == "reboot":
+            reboot_ec2s()
+        elif select == "4" or select.lower() == "terminate":
+            terminate_ec2()
+        elif select == "5" or select.lower() == "exit" or select.lower() == "quit":
+            print("GoodBye....."), sys.exit()
+        else:
+            print("Sorry, your entry was not valid.")
+            try_again = input("Do you want to try again?: ")
+            if try_again.lower() == "yes":
+                menu()
+            else:
+                print("GoodBye.....")
+
     def start_ec2s():
         get_instance = input("Please enter your instance id: ")
+        #if get_instance in menu():
+
         ec2_cli.start_instances(InstanceIds=[get_instance])
         return None
 
@@ -84,4 +90,3 @@ try:
         main()
 except Exception as e:
     print(e)
-
